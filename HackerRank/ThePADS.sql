6aba6869595deae9cb86ae8914e33349e11519c1
Palak Jain <141797620+pal1501@users.noreply.github.com>
2026-06-23 21:13:30 +0530

    Create ThePADS.sql with occupation queries
    Add SQL queries to generate ordered names and occupation counts.

---
+++ HackerRank/ThePADS.sql
@@ -0,0 +1,18 @@
+-- Solution
+
+/*
+Generate the following two result sets:
+
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them.
+*/
+
+SELECT CONCAT(name,'(', LEFT(occupation,1), ')')
+FROM occupations
+ORDER BY name ASC;
+
+SELECT CONCAT('There are a total of ',COUNT(*),' ',LOWER(occupation),'s.')
+FROM occupations
+GROUP BY occupation
+ORDER BY COUNT(*) ASC, occupation ASC;
